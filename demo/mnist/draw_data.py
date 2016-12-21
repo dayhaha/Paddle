5a9f53f0b63dfc259c7ed70c1ba3f9348e03bc9f
dayhaha <18800111918@163.com>
2016-12-21 16:28:52 +0800

    modify draw_data.py and add example image

--- demo/mnist/draw_data.py
+++ demo/mnist/draw_data.py
@@ -1,3 +1,16 @@
+# Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import numpy as np
 import matplotlib.pyplot as plt
 import random
@@ -18,8 +31,12 @@ def read_data(path, filename):
     else:
         n = 10000
 
+    rows = 28
+    cols = 28
+
     images = np.fromfile(
-        f, 'ubyte', count=n * 28 * 28).reshape((n, 28 * 28)).astype('float32')
+        f, 'ubyte',
+        count=n * rows * cols).reshape(n, rows, cols).astype('float32')
     labels = np.fromfile(l, 'ubyte', count=n).astype("int")
 
     return images, labels
@@ -30,7 +47,7 @@ if __name__ == "__main__":
     test_images, test_labels = read_data("./data/raw_data/", "t10k")
     label_list = []
     for i in range(10):
-        index = random.randint(0, len(train_images) - 1)
+        index = random.randint(0, train_images.shape[0] - 1)
         label_list.append(train_labels[index])
         plt.subplot(1, 10, i + 1)
         plt.imshow(train_images[index], cmap="Greys_r")
